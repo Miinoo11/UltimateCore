cf7ab8718ebf23a5d4fe5bc2266c4fd81b35b1ca
Tamino Höhne <kaylooyt@gmail.com>
2021-11-15 14:07:45 +0100

    Added 1_8_R3 Module

--- adapter/src/main/java/dev/miinoo/ucore/version/VersionWrapper.java
+++ adapter/src/main/java/dev/miinoo/ucore/version/VersionWrapper.java
@@ -4,11 +4,29 @@ import org.bukkit.entity.Player;
 
 public interface VersionWrapper {
 
+    /**
+     * @param player the anvilui is created for
+     * @return AnvilUI
+     * @see dev.miinoo.ucore.menu.AnvilUI
+     */
     Object createAnvilUI(Player player);
 
+    /**
+     * @param player the tablist is displayed to
+     * @param header string that is displayed at the top of the player tab list
+     * @param footer string that is displayed below the player tab list
+     */
     void sendTabListHeaderFooter(Player player, String header, String footer);
 
-    Object addGlow(boolean bool);
+    /**
+     * @param show
+     * @return ItemBuilder
+     */
+    Object addGlow(boolean show);
 
+    /**
+     *
+     * @return ItemBuilder
+     */
     Object addGlow();
 }
--- ucore/src/main/java/dev/miinoo/ucore/version/VersionMatcher.java
+++ ucore/src/main/java/dev/miinoo/ucore/version/VersionMatcher.java
@@ -7,7 +7,7 @@ public class VersionMatcher {
     public VersionWrapper match() {
         final String serverVersion = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3].substring(1);
         try {
-            return (VersionWrapper) Class.forName(getClass().getPackage().getName() + ".Wrapper" + serverVersion).newInstance();
+            return (VersionWrapper) Class.forName(getClass().getPackage().getName() + ".AnvilGUI_v" + serverVersion).newInstance();
         } catch (IllegalAccessException | InstantiationException exception) {
             throw new IllegalStateException("Failed to create version wrapper for version " + serverVersion, exception);
         } catch (ClassNotFoundException exception) {
